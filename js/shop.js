429f5a6fb42f757bfa94e44450e978db969d15a4
Saraebba <98426121+Saraebba@users.noreply.github.com>
2023-02-11 22:47:16 +0100

    .

--- js/shop.js
+++ js/shop.js
@@ -3,21 +3,10 @@ import productjson from '../products.json' assert {type:'json'};
 const products = [];
 let cart = [];
 const productlist = document.querySelector("#productlist");
-const modaltoggle = document.querySelector("#modal");
 
 
 if (localStorage.getItem("cart")) {
     cart = JSON.parse(localStorage.getItem("cart"));
-}
-
-const openEls = document.querySelectorAll("[data-open]");
-const isVisible = "is-visible";
-
-for(const el of openEls) {
-  el.addEventListener("click", function() {
-    const modalId = this.dataset.open;
-    document.getElementById('modal').classList.add(isVisible);
-  });
 }
 
 class Product{
@@ -66,19 +55,11 @@ function listProducts(){
     //lägg till innehåll
     img.src = `${product.bouqetimg}`;
     productname.innerText = `${product.bouqet}`;
-    // modallink.innerText = "läs mer..."
     info.innerText = `${product.info}`;
     price.innerText = `${product.price} $`;
     addtocart.innerText ="Add to cart";
 
 
-    //lägg till 
-    // modalheader.append(productname)
-    // modalbody.append(info)
-    // modalfooter.append(price, addtocart)
-    // modalcontent.append(modalheader, modalbody, modalfooter)
-    // modaldialog.append(modalcontent)
-    // modalfade.append(modaldialog)
     cardbody.append(img, productname, info, price, addtocart);
     card.append(cardbody);
     div.append(card);
